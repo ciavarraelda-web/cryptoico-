215bbebdac09f485bcdc450efe15020d338b31cb
ciavarraelda-web <ciavarraelda@gmail.com>
2025-08-30 14:48:47 +0200

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,14 @@
+// app/layout.tsx (o nella directory della tua pagina)
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
