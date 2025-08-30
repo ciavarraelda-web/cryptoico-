d724726a4ada7cb0ce121ff67586f676411ccccc
ciavarraelda-web <ciavarraelda@gmail.com>
2025-08-30 14:54:13 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@
 const nextConfig = {
   // Configurazioni specifiche per la build
   typescript: {
-    // Ignora gli errori TypeScript durante la build
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Ignora gli errori ESLint durante la build
     ignoreDuringBuilds: true,
   },
 }
