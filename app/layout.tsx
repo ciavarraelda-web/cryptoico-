e8f931429f18a6b3e5d825d81b5a227b256c0b98
ciavarraelda-web <ciavarraelda@gmail.com>
2025-08-30 14:52:55 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+// app/layout.tsx
+import './globals.css';
+
+export const metadata = {
+  title: 'CryptoICO',
+  description: 'La tua piattaforma per investimenti in criptovalute',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="it">
+      <body>{children}</body>
+    </html>
+  );
+}
